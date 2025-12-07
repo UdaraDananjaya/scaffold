1b05ce87948fe4db8b0de86fedeec5c7690e664b
Udara Dananjaya <udarapaymedia@gmail.com>
2025-12-07 22:10:51 +0530

    Add SecurityConfig for Disable the all endpoints

--- src/main/java/lk/gtsactive/scaffold/config/SecurityConfig.java
+++ src/main/java/lk/gtsactive/scaffold/config/SecurityConfig.java
@@ -2,17 +2,29 @@ package lk.gtsactive.scaffold.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                );
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+                //.httpBasic(httpBasic -> httpBasic.disable())
+                //.formLogin(form -> form.disable());
+
+        return http.build();
     }
 }
