08adff725af7ec06133a15601c58b0e0bc29765b
Udara Dananjaya <udarapaymedia@gmail.com>
2025-12-07 14:43:58 +0530

    Initial Commit

---
+++ src/test/java/lk/gtsactive/scaffold/ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.gtsactive.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
